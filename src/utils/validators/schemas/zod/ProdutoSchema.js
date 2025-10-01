96247439b4ee7c673117d1dc05996540d8185c2e
Matheus Batista <matheusifro2020@gmail.com>
2025-09-30 21:54:43 -0400

    fix: ajuste no filtro de estoque baixo

--- src/utils/validators/schemas/zod/ProdutoSchema.js
+++ src/utils/validators/schemas/zod/ProdutoSchema.js
@@ -14,11 +14,7 @@ export const ProdutoSchema = z.object({
     }).optional(),
     estoque: z.number().int('Estoque deve ser um número inteiro').nonnegative('Estoque não pode ser negativo').optional(),
     estoque_min: z.number().int('Estoque mínimo deve ser um número inteiro').nonnegative('Estoque mínimo não pode ser negativo'),
-    data_ultima_entrada: z.preprocess(
-        (arg) => arg === undefined ? undefined : arg instanceof Date ? arg : new Date(arg),
-        z.date().optional()
-    ),
-    // status: z.boolean().optional(),
+    status: z.boolean().optional(),
     fornecedores: z.string()
     .refine(isValidObjectId, {
       message: "ID do fornecedor inválido",
